54eaee017a3c6c3dc1e1095a5b9da12d6f1a68f2
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-09-28 21:26:44 +0900

    tweetテーブルのnicknameカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_28_093438) do
+ActiveRecord::Schema.define(version: 2022_09_28_122433) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
