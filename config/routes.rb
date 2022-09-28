f501f16cb4668bfd5ff3cc91afa895c957a55ff5
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-09-28 12:47:18 +0900

    ビューまで実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  root to: 'tweets#index'
+  #root to:パス名で標準URLに設定
   resources :tweets
   #resources で７つのアクションルートを自動作成
   #resources :tweets, only: [:index, :new]と書くと二つのみルーティング設定できる
