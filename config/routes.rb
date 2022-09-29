c6905314b8bc329e60a8e10f093d5eb3e6f801f8
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-09-29 16:42:33 +0900

    commentコントローラー実装・リダイレクト記入

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   #root to:パス名で標準URLに設定
-  resources :tweets
+  resources :tweets do
   #resources で７つのアクションルートを自動作成
+    resources :comments, only: :create
+  end
   #resources :tweets, only: [:index, :new]と書くと二つのみルーティング設定できる
   resources :users, only: :show
 
